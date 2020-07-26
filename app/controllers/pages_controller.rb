84efa84a510ac20c3f4c15e0c8a41eda405d00f9
Ismael <ismael.br187@gmail.com>
2020-07-26 18:42:49 -0400

    postgreSQL for production

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+  http_basic_authenticate_with name: "tunombre", password: "tupassword", only: :dashboard
+
+  def index
+  end
+
+  def create
+    @user = User.create(email: params[:email], age: params[:age], comment: params[:comment])
+  end
+
+  def dashboard
+    if params[:email].present?
+      @users = User.where('email = ?', params[:email])
+    else
+      @users = User.find_valid
+    end
+  end
+end
